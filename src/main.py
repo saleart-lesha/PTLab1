eb88eba1b69087f220b099ac038ce78aad64ac92
Aleksey Svishchev <63710687+saleart-lesha@users.noreply.github.com>
2023-11-10 17:23:35 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import sys
 
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from StudentCalculator import StudentCalculator
+from YAMLDataReader import YAMLDataReader
 
 
 def get_path_from_arguments(args) -> str:
@@ -12,15 +14,25 @@ def get_path_from_arguments(args) -> str:
     args = parser.parse_args(args)
     return args.path
 
+def get_current_reader(path: str) -> DataReader:
+    _, file_extension = os.path.\
+        splitext(path)
+    match file_extension:
+        case ".txt":
+            return TextDataReader()
+        case ".yaml":
+            return YAMLDataReader()
+        case _:
+            raise ValueError("Неподдерживаемый формат")
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
-    reader = TextDataReader()
+    reader = get_current_reader(path)
     students = reader.read(path)
     print("Students: ", students)
-
-    rating = CalcRating(students).calc()
-    print("Rating: ", rating)
+    excellent_students = StudentCalculator().\
+        count_excellent_students(students)
+    print("Excellent students : ", excellent_students)
 
 
 if __name__ == "__main__":
